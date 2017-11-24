80873315a6771b11b337ce030ba0cb5a7ea9607a
George <pavlheo@gmail.com>
2017-11-24 04:40:17 +0200

    enzyme is added; few unit tests for components

--- user-manager-client/src/components/App.test.tsx
+++ user-manager-client/src/components/App.test.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import Routes from "./Routes";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const enzyme = require("enzyme");
+const adapter =  require('enzyme-adapter-react-16');
+enzyme.configure({adapter: new adapter()})
+describe("App component", ()=>{
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it("has 1 provider and 1 route", () => {
+        const wrapper = enzyme.shallow(<App />);
+        expect(wrapper.find(Provider)).toHaveLength(1);
+        expect(wrapper.find(Routes)).toHaveLength(1);
+    });
 });
